ddf3794a343f729989f8f03bd7cf8db1823f916f
Slobodan Sesto <Slobodan.600000@gmail.com>
2020-02-27 18:05:43 -0500

    updating api config

--- src/api/config.js
+++ src/api/config.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 var instance = axios.create({
-    baseURL: process.env.VUE_APP_API,
+    baseURL: process.env.VUE_APP_API + "api",
   })
 
 export default instance;
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,19 +1,24 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <img alt="Vue logo" :src='basePath + "images/sd_5dd492b2da76d.jpg"'>
+    <!-- <HelloWorld msg="Welcome to Your Vue.js App"/> -->
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+// import HelloWorld from '@/components/HelloWorld.vue'
 import api from '../api/index'
 
 export default {
   name: 'Home',
+  data() {
+    return {
+      basePath: process.env.VUE_APP_API
+    }
+  },
   components: {
-    HelloWorld
+    // HelloWorld
   },
   mounted() {
     api.testing().then( res => {
