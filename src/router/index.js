71b6f65f19d66736831f642c064977f6078a5b85
Slobodan Sesto <Slobodan.600000@gmail.com>
2020-02-29 07:00:05 -0500

    tab name

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 import Home from '../views/Home.vue'
+import Contact from '../views/Contact.vue'
 import Gallery from '../views/Gallery.vue'
 import Order from '../views/Order.vue'
 
@@ -30,6 +31,7 @@ const routes = [
 ]
 
 const router = new VueRouter({
+  mode: 'history',
   routes
 })
 
--- src/views/Order.vue
+++ src/views/Order.vue
@@ -1,11 +1,21 @@
 <template>
   <div class="order">
-    <h3>How to send us injectors?</h3>
-    <div class="directions">
+    <h3 class="order-h">How to send us injectors?</h3>
+    <div class="note">
       <p>We will be closed Friday June 7th - June 17th. If you are able to wait until we come back you are welcome to send the injectors to us.</p>
     </div>
     <div class="order-form">
       Please fill out the<a :href='ordertUrl' target="_blank"> Order form</a>
+    </div>
+    <div class="payment-links">
+      
+    </div>
+    <div class="ship">
+      Once that is completed send the injectors to the following address: <br/><br/>
+      ATTN: Cleveland Injector Hospital<br/>
+      14626 N. Gallatin Blvd<br/>
+      Brook Park, OH 44142<br/>
+      216-219-2263<br/>
     </div>
   </div>
 </template>
@@ -25,5 +35,14 @@ export default {
 </script>
 
 <style scoped>
+.order {
+  padding: 5%;
+}
+.order-h {
+  font-size: 3em;
+  margin-bottom: 20px;
+}
+.note {
 
+}
 </style>
