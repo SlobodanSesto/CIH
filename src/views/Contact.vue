54c3d459b3bee0cc1b0c1711c8048282fa662b49
Slobodan Sesto <Slobodan.600000@gmail.com>
2020-03-06 08:15:19 -0500

    input fields and simple form validation

--- src/views/Contact.vue
+++ src/views/Contact.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="about">
+  <div class="contact">
     <div class="left-side">
       <i class="material-icons">question_answer</i> FAQ
       <ul>
@@ -10,47 +10,84 @@
       </ul>
     </div>
     <div class="right-side">
-      <form>
-        <div class="form-group">
-          <label for="exampleFormControlInput1">Email address</label>
-          <input type="email" class="form-control" id="exampleFormControlInput1" placeholder="name@example.com">
-        </div>
-        <div class="form-group">
-          <label for="exampleFormControlInput1">Phone</label>
-          <input type="text" class="form-control" id="exampleFormControlInput2" placeholder="name@example.com">
-        </div>
-        <div class="form-group">
-          <label for="exampleFormControlInput1">Name</label>
-          <input type="text" class="form-control" id="exampleFormControlInput3" placeholder="name@example.com">
-        </div>
-        <div class="form-group">
-          <label for="exampleFormControlSelect1">Example select</label>
-          <select class="form-control" id="exampleFormControlSelect1">
-            <option>1</option>
-            <option>2</option>
-            <option>3</option>
-            <option>4</option>
-            <option>5</option>
-          </select>
-        </div>
-        <div class="form-group">
-          <label for="exampleFormControlTextarea1">Message</label>
-          <textarea class="form-control" id="exampleFormControlTextarea1" rows="3"></textarea>
-        </div>
-        <button type="submit" class="btn">Submit</button>
-      </form>
+      <div class="form-group">
+        <label for="name">Name</label>
+        <input type="text" class="form-control" id="name" v-model="cName">
+      </div>
+      <div class="form-group">
+        <label for="email">Email address</label>
+        <input type="email" class="form-control" id="email" placeholder="name@example.com" v-model="cEmail">
+      </div>
+      <input type="hidden" name="_honeypot" @change="honeypot" v-model="cHoneypot">
+      <div class="form-group">
+        <label for="phone">Phone</label>
+        <input type="text" class="form-control" id="phone" placeholder="555-555-5555" v-model="cPhone">
+      </div>
+      <div class="form-group">
+        <label for="msg">Message</label>
+        <textarea class="form-control" id="msg" rows="3" v-model="cMsg"></textarea>
+      </div>
+      <button @click="submitForm" class="btn btn-secondary">Submit</button>
     </div>
   </div>
+
+
+      <!-- <div class="form-group">
+        <label for="exampleFormControlSelect1">Example select</label>
+        <select class="form-control" id="exampleFormControlSelect1">
+          <option>1</option>
+          <option>2</option>
+          <option>3</option>
+          <option>4</option>
+          <option>5</option>
+        </select>
+      </div> -->
 </template>
 
 <script>
 export default {
-  
+  name: 'Contact',
+  data() {
+    return {
+      cHoneypot: '',
+      cEmail: '',
+      cPhone: '',
+      cName: '',
+      cMsg: '',
+    }
+  },
+  methods: {
+    honeypot() {
+      console.log('honeypot val changed')
+    },
+    submitForm() {
+      let msg = this.validateInput();
+      if(msg) {
+        console.log(msg)
+      }
+      // console.log(this.cName)
+      // console.log(this.cHoneypot)
+      // console.log(this.cPhone)
+      // console.log(this.cEmail)
+      // console.log(this.cMsg)
+      // console.log('form submit!')
+    },
+    validateInput() {
+      if(this.cName === '' || this.cPhone === '' || this.cEmail === '' || this.cMsg === '') {
+        return "Please complete all fields"
+      }
+      var email_regex = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/i;
+      if(!email_regex.test(this.cEmail)){
+        console.log("invalid email")
+        return 'Invalid e-mail'
+      }
+    }
+  }
 }
 </script>
 
 <style scoped>
-.about {
+.contact {
   padding: 5%;
   display: flex;
   flex-direction: row;
